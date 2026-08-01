b8db32df5e5e229e9eeb211ec3897acba42631a2
Furqan Khalid <furqaanmalik071@gmail.com>
2026-08-01 12:59:10 +0500

    day 17 color changer mini project

---
+++ web dev/Day-17.js
@@ -0,0 +1,29 @@
+document.getElementById("btn1").addEventListener("click", function() 
+{
+    document.getElementById("title").innerHTML = "Current Background Color Pink";
+    document.body.style.backgroundColor = "pink";
+});
+
+document.getElementById("btn2").addEventListener("click", function()
+{
+    document.getElementById("title").innerHTML = "Current Background Color Orange";
+    document.body.style.backgroundColor = "orange";
+});
+
+document.getElementById("btn3").addEventListener("click", function()
+{
+     document.getElementById("title").innerHTML = "Current Background Color Yellow";
+    document.body.style.backgroundColor = "yellow";
+});
+
+document.getElementById("btn4").addEventListener("click", function()
+{
+    document.getElementById("title").innerHTML = "Current Background Color Green";
+    document.body.style.backgroundColor = "green";
+});
+
+document.getElementById("btn5").addEventListener("click", function()
+{
+    document.getElementById("title").innerHTML = "Current Background Color Purple";
+    document.body.style.backgroundColor = "purple";
+});
