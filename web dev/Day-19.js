c61dfde2aa218b83a08528683d986e532280131b
Furqan Khalid <furqaanmalik071@gmail.com>
2026-08-05 16:46:05 +0500

    Day 19 grade calculator

---
+++ web dev/Day-19.js
@@ -0,0 +1,23 @@
+document.getElementById("calculateBtn").addEventListener("click", function() {
+    let name = document.getElementById("name").value;
+    let marks1 = Number(document.getElementById("marks1").value);
+    let marks2 = Number(document.getElementById("marks2").value);
+    let marks3 = Number(document.getElementById("marks3").value);
+
+    let student = {
+        name: name,
+        marks: [marks1, marks2, marks3],
+        average: (marks1 + marks2 + marks3) / 3,
+        getGrade: function() {
+            if (this.average >= 80) return "A";
+            else if (this.average >= 60) return "B";
+            else if (this.average >= 40) return "C";
+            else return "F";
+        }
+    };
+
+    document.getElementById("result").innerHTML = 
+        "Student: " + student.name + "<br>" +
+        "Average: " + student.average.toFixed(2) + "<br>" +
+        "Grade: " + student.getGrade();
+});
